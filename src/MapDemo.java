210db256dbeb1a0d16f0f7ece27c6ffd2dc65165
Allison Snipes <ajsnipes@usfca.edu>
2020-08-10 12:27:57 -0700

    first commit

--- src/MapDemo.java
+++ src/MapDemo.java
@@ -1,8 +1,20 @@
-
+/**
+ * Allison Snipes
+ * Course 605.201.81 Summer 2020
+ * Assignment 11 Question 1
+ * 
+ *  Project Specs:
+ *  Supplied is a data file from the US Census which contains data from US school districts and reports 
+ *  statistics related to child poverty (http://www.census.gov/did/www/saipe/data/highlights/2013.html). 
+ *  It is desired to have a summary report which calculate basic statistics at the state level.
+ *  
+ *  @author Allison Snipes
+ *  @version 1.0 
+ */
 public class MapDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("works.");
 
 	}
 
